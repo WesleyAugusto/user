404af7d4fe766e410a7f7c13e3b05e029a049085
WesleyAugusto <ranzerap@gmail.com>
2022-10-26 20:41:17 -0300

    get parameter

--- src/main/kotlin/br/com/user/UserController.kt
+++ src/main/kotlin/br/com/user/UserController.kt
@@ -1,13 +1,14 @@
 package br.com.user
 
+import io.micronaut.context.annotation.Parameter
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 
 @Controller("/user")
 class UserController {
 
-    @Get("/get")
-    fun getUsers():String{
-        return "seja bem vindo"
+    @Get("/{name}")
+    fun getUsers(@Parameter name:String):String{
+        return "seja bem vindo $name"
     }
 }
