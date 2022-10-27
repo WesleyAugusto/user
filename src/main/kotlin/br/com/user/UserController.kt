53c6e749114e36d71ac4c7389ade2d28009a81d1
WesleyAugusto <ranzerap@gmail.com>
2022-10-26 21:01:30 -0300

    post body

--- src/main/kotlin/br/com/user/UserController.kt
+++ src/main/kotlin/br/com/user/UserController.kt
@@ -1,14 +1,26 @@
 package br.com.user
 
 import io.micronaut.context.annotation.Parameter
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 
 @Controller("/user")
 class UserController {
 
     @Get("/{name}")
-    fun getUsers(@Parameter name:String):String{
+    fun getUser(@Parameter name:String):String{
         return "seja bem vindo $name"
     }
+
+    @Post
+    fun postUser(@Body user:User ):String{
+        return "seja bem vindo ${user.email} ${user.name}"
+    }
 }
+
+data class User(
+    val email:String,
+    val name:String?
+)
