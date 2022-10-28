9f25e7a770f65075e7abc9e8bd54e6c617c400c2
WesleyAugusto <ranzerap@gmail.com>
2022-10-27 21:13:24 -0300

    validaçoes de usuario com if else

--- src/main/kotlin/br/com/user/service/UserService.kt
+++ src/main/kotlin/br/com/user/service/UserService.kt
@@ -2,18 +2,16 @@ package br.com.user.service
 
 import br.com.user.model.UserRequest
 
-class UserService{
+class UserService {
     fun validationUser(userRequest: UserRequest): String {
-        if(userRequest.age < 18){
-            return "Voce e menor de idade"
+        return if (userRequest.age < 18) {
+            "Voce e menor de idade"
+        } else if (userRequest.email?.contains("@") ?: false == false) {
+            "email invalido "
+        } else if (userRequest.name.length < 3) {
+            "nome invalido"
+        } else {
+            "parabens cadastrou"
         }
-        if(userRequest.email?.contains("@") ?: false == false){
-            return "email invalido "
-        }
-
-        if (userRequest.name.length < 3){
-            return "nome invalido"
-        }
-        return "parabens cadastrou"
     }
 }
